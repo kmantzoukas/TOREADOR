ea0f0b1309669eb7700891ef23b9729839b924ac
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-04-10 16:04:32 +0300

    Added findByStatus() method to fetch all newly created prism requests

--- PrismRequestConsumer/src/main/java/uk/ac/city/PrismRequestRepository.java
+++ PrismRequestConsumer/src/main/java/uk/ac/city/PrismRequestRepository.java
@@ -1,7 +1,12 @@
 package uk.ac.city;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+
+import uk.ac.city.PrismRequest;
 
 public interface PrismRequestRepository extends CrudRepository<PrismRequest, Long>{
 	PrismRequest findById(Long id);
+	List<PrismRequest> findByStatus(Status status);
 }
