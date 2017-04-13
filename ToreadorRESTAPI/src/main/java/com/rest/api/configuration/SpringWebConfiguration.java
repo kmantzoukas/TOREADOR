338a54ebefa7c2a164a2fd8a984fadb5fe3aabbd
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-04-13 15:12:01 +0300

    Added support for Swagger

---
+++ ToreadorRESTAPI/src/main/java/com/rest/api/configuration/SpringWebConfiguration.java
@@ -0,0 +1,14 @@
+package com.rest.api.configuration;
+
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class SpringWebConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+}
