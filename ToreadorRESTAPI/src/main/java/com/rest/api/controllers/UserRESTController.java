01b67b3eb08b97cfbb2c0719e85cb29a6a2be1ab
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-04-13 15:12:31 +0300

    Added support for Swagger

--- ToreadorRESTAPI/src/main/java/com/rest/api/controllers/UserRESTController.java
+++ ToreadorRESTAPI/src/main/java/com/rest/api/controllers/UserRESTController.java
@@ -1,5 +1,10 @@
 package com.rest.api.controllers;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -14,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rest.api.entities.User;
 import com.rest.api.jpa.repositories.UserRepository;
 
-
+@Api(tags= "User resource")
 @RestController
 public class UserRESTController {
 	
@@ -27,8 +32,13 @@ public class UserRESTController {
 	/*
 	 * This method returns a list of all the users stored in the database
 	 */
-	@RequestMapping(value= "/rest/api/users", method = RequestMethod.GET)
-	public ResponseEntity<?> getAllUsers() {
+	@ApiOperation(value = "Return a list of all the users in the database", nickname = "getAllUsers")
+	@ApiResponses({
+	    @ApiResponse(code =  404, message ="Not found"),
+	    @ApiResponse(code =  400, message ="Invalid input")
+	})
+	@RequestMapping(value= "/rest/api/users", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<List<User>> getAllUsers() {
 		
 		List<User> users = null;
 		
@@ -45,18 +55,35 @@ public class UserRESTController {
 	/*
 	 * This method returns a user with the specified id that is stored in the database
 	 */
-	@RequestMapping(value = "/rest/api/users/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> getUserById(@PathVariable Long id) {
+	@ApiOperation(
+			value = "Return the user with the specified id from the database", 
+			nickname = "getUserById",
+			response = User.class)
+	@ApiResponses({
+	    @ApiResponse(code =  404, message ="The user with the specified id does was not found in the databse"),
+	    @ApiResponse(code =  400, message ="The user id provided is not in a valid format")
+	})
+	@RequestMapping(value = "/rest/api/users/{id}", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<User> getUserById(@PathVariable Long id) {
 		
 		User user = null;
 		
 		try {
 			user = repository.findById(id);
-			log.info("Fetching " + user.toString());
-			return new ResponseEntity<User>(user,HttpStatus.OK);
+			
+			if(user == null){
+				log.info("User with id " + id + " was not found in the database");
+				return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+			}
+				
+			else{
+				log.info("Fetching " + user.toString());
+				return new ResponseEntity<User>(user, HttpStatus.OK);
+			}
+			
 		} catch (Exception e) {
 			log.error(e.getMessage());
-			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<User>(user, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
