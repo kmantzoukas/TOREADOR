5974a065dfcc17b1db6671f6681926ee8171e41c
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-04-13 15:11:03 +0300

    Added support for Swagger

--- ToreadorRESTAPI/src/main/java/com/rest/api/configuration/WebInitializer.java
+++ ToreadorRESTAPI/src/main/java/com/rest/api/configuration/WebInitializer.java
@@ -6,7 +6,7 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { SpringConfiguration.class };
+        return new Class[] { SpringConfiguration.class, SpringWebConfiguration.class };
     }
   
     @Override
