5145d2ce0fec6e11526948e2f7cd7b7c51f771dc
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-04-13 14:58:07 +0300

    Added ApiModel

--- ToreadorRESTAPI/src/main/java/com/rest/api/entities/PrismRequest.java
+++ ToreadorRESTAPI/src/main/java/com/rest/api/entities/PrismRequest.java
@@ -1,5 +1,8 @@
 package com.rest.api.entities;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 import java.sql.Timestamp;
 
@@ -14,29 +17,43 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
+@ApiModel
 @Entity
 @Table(name = "prism_requests", catalog = "toreador")
 public class PrismRequest implements Serializable{
 	
 	private static final long serialVersionUID = -5757552134175485875L;
 	
+	@ApiModelProperty(required = false, example = "36")
 	@Id
 	@GeneratedValue
 	@Column(name="id", unique=true, nullable=false)
 	private Long id;
-	@Column(name="output_file", nullable=false)
+	
+	@ApiModelProperty(example = "This is some test content of the prism model", required = true)
+	@Column(name="model", nullable=false)
+	private String model;
+	
+	@ApiModelProperty(example = "This is some test content for the properties file", required = true)
+	@Column(name="properties", nullable=false)
+	private String properties;
+	
+	@ApiModelProperty(example = "/path/to/output/file.txt", required = true)
+	@Column(name="output", nullable=false)
 	private String output;
+	
+	@ApiModelProperty(required = false, example = "1492073991")
 	@Column(name="created", nullable=false)
 	private Timestamp created;
+	
+	@ApiModelProperty(example = "CREATED", allowableValues = "CREATED,PROCESSING,COMPLETED,ERROR")
 	@Column(name="status", nullable=false)
 	@Enumerated(EnumType.STRING)
 	private Status status;
 	
+	@ApiModelProperty(required=true, hidden = false)
 	@ManyToOne(optional = false, fetch=FetchType.EAGER)
 	@JoinColumn(name = "user")
-	@JsonIgnore
 	private User user;
 	
 	public Long getId() {
