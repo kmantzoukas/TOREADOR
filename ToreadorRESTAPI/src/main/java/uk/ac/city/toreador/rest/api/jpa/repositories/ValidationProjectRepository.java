58d5bda0ea91dfc67b56035ff9ad99771923ca57
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-05-25 12:08:32 +0300

    Rename PrismRequestRepository to ValidationProjectRepository

---
+++ ToreadorRESTAPI/src/main/java/uk/ac/city/toreador/rest/api/jpa/repositories/ValidationProjectRepository.java
@@ -0,0 +1,13 @@
+package uk.ac.city.toreador.rest.api.jpa.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import uk.ac.city.toreador.rest.api.entities.User;
+import uk.ac.city.toreador.rest.api.entities.ValidationProject;
+
+public interface ValidationProjectRepository extends CrudRepository<ValidationProject, Long>{
+	ValidationProject findById(Long id);
+	List<ValidationProject> findByUser(User user);
+}
