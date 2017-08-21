73cff64728f5821f4512f8736c24a4fe3e4add88
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-08-22 02:43:26 +0300

    Added functions that represent atomic services. The functions are being invoked in a specific order in order to implement the composite service.

--- LightSourceBDAServices/src/main/scala/uk/ac/city/services/AverageConsumptionGroupByKey.scala
+++ LightSourceBDAServices/src/main/scala/uk/ac/city/services/AverageConsumptionGroupByKey.scala
@@ -1,58 +1,99 @@
 package uk.ac.city.services
 
+import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.SparkSession
+
+import org.apache.spark.rdd.RDD
+
 import uk.ac.city.Model._
 import uk.ac.city.listeners.SLAListener
 
 object AverageConsumptionGroupByKey {
-	
+
   val spark = SparkSession.builder.getOrCreate
-  
+  import spark.implicits._
+
   def main(args: Array[String]) {
-    
-    import spark.implicits._
-    
+
     /*
      * Added an SLA listener on the SparkContext
      */
     spark.sparkContext.addSparkListener(new SLAListener)
+
+    val path: String = args(0)
     
-    /*
-     * Read all the measurements and place the in Dataset[Measurement]
-     */
-    val measurements = spark.read.json(args(0)).as[Measurement]
-    
-    /*
-     * Flatten all values to combine the gateway id with the appliance type and the corresponding value of consumption
-     */
-    measurements.flatMap(_.explode)
-    /*
-     * Map the measurements Dataset into an RDD with key:(id,applianceType) and value:consumption
-     */
-    .map{
-      case (id,applianceType,consumption) => ((id,applianceType),consumption) 
-    }
-    
-    /*
-     * Convert measurements into and RDD and group them by key which the combination of id and appliance type
-     */
-    .rdd
-    
-    /*
-     * Group the measurements by (id,applianceType)
-     */
-    .groupByKey()
-    
-    /*
-     * Perform a map operation to calculate the final result
-     */
-    .map{
-      case ((id,applianceType),consumption) => ((id,applianceType),consumption.sum / consumption.size)
-    }
-    
-    .collect.foreach(println)
-    
+    printResults(
+        collectResults(
+            calculateAverages(
+                groupByKey(
+                    convertToRDD(
+                        createKeyValuePairs(
+                            explode(
+                                load(path))))))))
+
     spark.stop
   }
+
+  /*
+   * Load the data from a data source. In our case from a JSON file
+   */
+  def load(path: String): Dataset[Measurement] = {
+    spark.read.json(path).as[Measurement]
+  }
+
+  /*
+   * Explode the data for the appliances for each measurement
+   */
+  def explode(measurements: Dataset[Measurement]): Dataset[(String, String, Double)] = {
+    measurements.flatMap(_.explode)
+  }
+
+  /*
+   * Create the (Key,Value) pairs where the key is the combination of the gateway id and the appliance type and the value is the
+   * energy consumption. An example would look like this: (10,STOVE), 382.38)
+   */
+  def createKeyValuePairs(dataset: Dataset[(String, String, Double)]): Dataset[((String, String), Double)] = {
+    dataset.map {
+      case (id, applianceType, consumption) => ((id, applianceType), consumption)
+    }
+  }
   
+  /*
+   * Convert the data set into and RDD on which we can apply Apache Spark's API functions such as groupByKey, CombineByKey, etc.
+   */
+  def convertToRDD(dataset: Dataset[((String, String), Double)]):RDD[((String, String), Double)] = {
+    dataset.rdd
+  }
+  /*
+   * Group the items of the RDD by key
+   */
+  def groupByKey(dataset:RDD[((String, String), Double)]):RDD[((String, String), Iterable[Double])] = {
+    dataset.groupByKey
+  }
+  
+  /*
+   * Calculate the averages for each key by adding the values and dividing them with the size of the  array that holds the respective values
+   */
+  def calculateAverages(dataset:RDD[((String, String), Iterable[Double])]):RDD[((String, String), Double)] = {
+    dataset.map {
+        case ((id, applianceType), consumption) => ((id, applianceType), consumption.sum / consumption.size)
+      }
+  }
+  
+  /*
+   * Collect the array of results. The results will be represented as an array of <Key,Value> pairs
+   * where the key is the combination of the gateway id and the appliance and the value is 
+   * the average energy consumption
+   */
+  def collectResults(dataset:RDD[((String, String), Double)]):Array[((String, String), Double)] = {
+   dataset.collect
+  }
+  
+  /*
+   * Print the results on the screen
+   */
+  def printResults(results:Array[((String, String), Double)]) = {
+   results.foreach(println)
+  }
+
 }
