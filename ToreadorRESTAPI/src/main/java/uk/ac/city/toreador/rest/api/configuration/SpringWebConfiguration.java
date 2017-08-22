6510d160a39e498bfd1813e5bcee7e01f409a87a
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-08-22 22:52:22 +0300

    Added a JSON converter to enable FAIL_ON_EMPTY_BEANS property

--- ToreadorRESTAPI/src/main/java/uk/ac/city/toreador/rest/api/configuration/SpringWebConfiguration.java
+++ ToreadorRESTAPI/src/main/java/uk/ac/city/toreador/rest/api/configuration/SpringWebConfiguration.java
@@ -1,14 +1,43 @@
 package uk.ac.city.toreador.rest.api.configuration;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 public class SpringWebConfiguration extends WebMvcConfigurerAdapter {
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
-	
+
+	@Bean
+	public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+		objectMapper.setSerializationInclusion(Include.NON_NULL);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		
+		jsonConverter.setObjectMapper(objectMapper);
+		return jsonConverter;
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(customJackson2HttpMessageConverter());
+		super.configureMessageConverters(converters);
+	}
+
 }
