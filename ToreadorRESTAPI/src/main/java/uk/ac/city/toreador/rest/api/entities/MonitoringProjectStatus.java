3bd81025c2966a657997577af3b902a5c5616b51
Constantinos Mantzoukas <Konstantinos.Mantzoukas.1@city.ac.uk>
2017-08-22 22:57:19 +0300

    Removed everything related to the monitoring project.

--- ToreadorRESTAPI/src/main/java/uk/ac/city/toreador/rest/api/entities/MonitoringProjectStatus.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.city.toreador.rest.api.entities;
-
-import io.swagger.annotations.ApiModel;
-
-@ApiModel
-public enum MonitoringProjectStatus {
-	CREATED,STARTED,PAUSED,FINISHED,ERROR
-}
